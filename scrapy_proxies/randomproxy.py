796db057e8f95585f776709ed66120890b0e7026
hzy <599012428@qq.com>
2021-02-21 02:30:09 +0800

    h

--- scrapy_proxies/randomproxy.py
+++ scrapy_proxies/randomproxy.py
@@ -1,116 +1,59 @@
-# Copyright (C) 2013 by Aivars Kalvans <aivars.kalvans@gmail.com>
+# Define here the models for your spider middleware
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import requests
+from scrapy import signals
 
-import re
 import random
-import base64
-import logging
-from scrapy_proxies_config.client.py_cli import ProxyFetcher
-log = logging.getLogger('scrapy.proxies')
+
+from twisted.internet.error import TCPTimedOutError, TimeoutError
+
+from .lib.scrapy_proxies_config.client.py_cli import ProxyFetcher
 
 
 
 
-class Mode:
-    RANDOMIZE_PROXY_EVERY_REQUESTS, RANDOMIZE_PROXY_ONCE, SET_CUSTOM_PROXY = range(3)
 
 
-
-class RandomProxy(object):
+class FundspiderDownloaderMiddleware(object):
     # def get_redis_conn(**kwargs):
     #     host = kwargs.get('host', settings.get('REDIS_HOST'))
     #     port = kwargs.get('port', settings.get('REDIS_PORT'))
     #     db = kwargs.get('db', settings.get('DEFAULT_REDIS_DB'))
     #     password = kwargs.get('password', settings.get('REDIS_PASSWORD') )
     #     return redis.StrictRedis(host, port, db, password)
-    
+    def process_request_back(self, request, spider):
+        for proxy_type in self.proxy_types:
+            fetcher = ProxyFetcher(proxy_type, strategy=self.strategy, length=self.ip_level)
+            self.proxies += fetcher.get_proxies()
+        request.meta["proxy"] = random.choice(self.proxies)
+        # request.headers["Proxy-Authorization"] = xun.headers
+
     def __init__(self, settings):
         self.mode = settings.get('PROXY_MODE')
         # 这里从配置选取类型，是要https还是http
         self.proxy_types = settings.get('PROXY_TYPE_LIST', ['https', 'http'])
         self.chosen_proxy = ''
+        # ip代理列表
+        self.proxies = []
         # 选择ip的复用模式
         self.strategy = settings.get('strategy', 'greedy')
         # ip 成绩等级
-        self.ip_level = settings.get('IP_LEVEL',8)
-        for proxy_type in self.proxy_types
-            fetcher = ProxyFetcher(proxy_type, strategy=self.strategy, length=self.ip_level)
-             self.proxies += fetcher.get_proxies()
-        if self.mode == Mode.RANDOMIZE_PROXY_EVERY_REQUESTS or self.mode == Mode.RANDOMIZE_PROXY_ONCE:
-            if self.mode == Mode.RANDOMIZE_PROXY_ONCE:
-                self.chosen_proxy = random.choice(list(self.proxies))
-        elif self.mode == Mode.SET_CUSTOM_PROXY:
-            custom_proxy = settings.get('CUSTOM_PROXY')
-            self.proxies = {}
-            # 不符合格式
-            parts = re.match('(\w+://)([^:]+?:[^@]+?@)?(.+)', custom_proxy.strip())
-            if not parts:
-                raise ValueError('CUSTOM_PROXY is not well formatted')
-            if parts.group(2):
-                user_pass = parts.group(2)[:-1]
-            else:
-                user_pass = ''
-
-            self.proxies[parts.group(1) + parts.group(3)] = user_pass
-            self.chosen_proxy = parts.group(1) + parts.group(3)
+        self.ip_level = settings.get('IP_LEVEL', 8)
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.settings)
 
     def process_request(self, request, spider):
-        # Don't overwrite with a random one (server-side state for IP)
-        if 'proxy' in request.meta:
-            if request.meta["exception"] is False:
-                return
-        request.meta["exception"] = False
-        if len(self.proxies) == 0:
-            raise ValueError('All proxies are unusable, cannot proceed')
-
-        if self.mode == Mode.RANDOMIZE_PROXY_EVERY_REQUESTS:
-            proxy_address = random.choice(list(self.proxies.keys()))
-        else:
-            proxy_address = self.chosen_proxy
-
-        proxy_user_pass = self.proxies[proxy_address]
-
-        if proxy_user_pass:
-            request.meta['proxy'] = proxy_address
-            basic_auth = 'Basic ' + base64.b64encode(proxy_user_pass.encode()).decode()
-            request.headers['Proxy-Authorization'] = basic_auth
-        else:
-            log.debug('Proxy user pass not found')
-        log.debug('Using proxy <%s>, %d proxies left' % (
-                proxy_address, len(self.proxies)))
+        pass
 
     def process_exception(self, request, exception, spider):
-        if 'proxy' not in request.meta:
-            return
-        if self.mode == Mode.RANDOMIZE_PROXY_EVERY_REQUESTS or self.mode == Mode.RANDOMIZE_PROXY_ONCE:
-            proxy = request.meta['proxy']
-            try:
-                del self.proxies[proxy]
-            except KeyError:
-                pass
-            request.meta["exception"] = True
-            if self.mode == Mode.RANDOMIZE_PROXY_ONCE:
-                self.chosen_proxy = random.choice(list(self.proxies.keys()))
-            log.info('Removing failed proxy <%s>, %d proxies left' % (
-                proxy, len(self.proxies)))
+        if isinstance(exception, TimeoutError):
+            self.process_request_back(request, spider)  # 连接超时才启用代理ip机制
+            return request
+
+        elif isinstance(exception, TCPTimedOutError):
+            self.process_request_back(request, spider)
+            return request
